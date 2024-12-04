ccd7e0fa700733714f5936b6d88e8da88dafac37
rakib3719 <bannah76769@gmail.com>
2024-12-04 09:57:07 +0600

    testing

--- src/app/[component]/home/Banner.jsx
+++ src/app/[component]/home/Banner.jsx
@@ -3,15 +3,15 @@ import bannerImg from '@/asset/images/banner.webp'
 
 const Banner = () => {
     return (
-        <div className="lg:flex">
+        <div className="lg:flex max-w-[1500px] mx-auto px-12 bg-[#fff5ed] items-center">
             <section>
 <h1 className="font-montserrat font-bold text-3xl lg:text-5xl">Luxury Hotel</h1>
 <p className="font-lato">Immerse yourself in the epitome of luxury at Almaris, your premier destination in New York. Our hotel redefines elegance and comfort, offering an exquisite escape for discerning travelers.</p>
 
-<button></button>
+<button className="bg-[#ab8965]  text-white btn p-4 mt-8">   Book Your Stay </button>
 
             </section>
-            <section>
+            <section className="mt-48">
 
 
                 <Image  src={bannerImg} alt="Banner"/>
